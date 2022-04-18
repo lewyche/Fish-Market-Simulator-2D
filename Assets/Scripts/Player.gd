0bd8c97ba537091e1a732f6ed48b3f63ae2f4c7f
TheDynamicVector <63025401+TheDynamicVector@users.noreply.github.com>
2022-04-18 16:14:03 -0700

    Player Animations and Inventory

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -1,22 +1,7 @@
-extends KinematicBody
+extends KinematicBody2D
 
-var velocity = Vector3.ZERO
-export var speed = 3500
-export var gravity = -136
-export var jump_force = 45
-var grounded = true
-
-
-var cam_rot = Vector3.ZERO
-
-export var mouse_sensitivity = 0.2
-
-
-onready var ground_raycast : RayCast = $RayCast
-onready var camera_pivot : Spatial = $Camera_Pivot
-
-
-onready var anim_tree = $AnimationTree
+var velocity = Vector2.ZERO
+export var speed = 30000
 
 
 var menu_open = false
@@ -29,69 +14,20 @@ func _physics_process(delta):
 	
 	#INPUT
 	#########################################
-	var direction = Vector3.ZERO
-	direction.x = Input.get_action_strength("Forward") - Input.get_action_strength("Back")
-	direction.z = -Input.get_action_strength("Left") + Input.get_action_strength("Right")
+	var direction = Vector2.ZERO
+	direction.y = -Input.get_action_strength("Up") + Input.get_action_strength("Down")
+	direction.x = -Input.get_action_strength("Left") + Input.get_action_strength("Right")
 	
 	direction = direction.normalized()
 	
+	velocity = direction * speed * delta
+	velocity = move_and_slide(velocity)
 	
-	#GRAVITY AND JUMP
-	#########################################
-	grounded = true if ground_raycast.is_colliding() else false
-	
-	if Input.is_action_just_pressed("Jump") && grounded:
-		
-		velocity.y = jump_force
-		
-		anim_tree.set("parameters/JumpShot/active", true)
-	
-	velocity.y += gravity * delta
-	
-	
-	
-	#FINAL VELOCITY
-	#########################################
-	velocity = $Armature.transform.basis * direction * speed * delta + Vector3(0,velocity.y,0)
-	
-	velocity = move_and_slide(velocity, Vector3.UP)
-	
-	
-	#ROTATE CAMERA
-	#########################################
-	camera_pivot.rotation_degrees.z = clamp(cam_rot.x, -60, 25)
-	camera_pivot.rotation_degrees.y = cam_rot.y
-	
-	
-	
-	#ROTATE PLAYER TO CAMERA
-	#######################################
-	
-	if Vector3(velocity.x,0,velocity.z) != Vector3.ZERO:
-		
-		$Armature.rotation_degrees.y = cam_rot.y
 	
 	
 	#PUNCH
 	######################################
 
-	if Input.is_action_just_pressed("Punch") && grounded:
-		
-		anim_tree.set("parameters/PunchShot/active", true)
+	if Input.is_action_just_pressed("Punch"):
+		print(1)
 	
-
-
-	#ANIMATIONS
-	#######################################
-	
-	
-	
-	anim_tree.set("parameters/Idle-Run/blend_position", velocity.length())
-	
-
-func _input(event):
-	
-	if event is InputEventMouseMotion && Input.MOUSE_MODE_CAPTURED:
-		
-		cam_rot.x += -event.relative.y*mouse_sensitivity
-		cam_rot.y += -event.relative.x*mouse_sensitivity
