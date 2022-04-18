f02b84391145159a1b1c9ad7e80d7a9f7a1db292
Robert <robertyan64913@gmail.com>
2022-04-18 16:27:55 -0400

    fix some keybind things and let player use mouse when opened panel

--- Nodes/World.gd
+++ Nodes/World.gd
@@ -5,8 +5,9 @@ var toggled = false
 func _input(event):
 	if toggled == false:
 		if Input.is_action_just_pressed("open_market"):
-			print("bruh")
 			toggled = true
+			
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			
 			get_node("Fish Market Panel").visible = true
 			get_node("Fish Market Panel").open_market()
@@ -17,6 +18,8 @@ func _input(event):
 			
 			get_node("Fish Market Panel").visible = false
 			get_node("Fish Market Panel").close_market()
+			
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		
 # Called when the node enters the scene tree for the first time.
 func _ready():
